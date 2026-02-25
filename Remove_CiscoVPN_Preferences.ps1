6355c6f5fca0a343b3b4c92aff44f40ef0c6b0bd
bbushman-IT <blbushman@digitechbench.com>
2026-02-25 06:29:17 -0500

    Add files via upload

---
+++ Remove_CiscoVPN_Preferences.ps1
@@ -0,0 +1,7 @@
+﻿#NOTE: PLEASE RUN THIS SCRIPT under CURRENT USER
+
+#Remove Preference XML File from Cisco Secure Client "CURRENT USER" (Helps with removing old VPN url )
+Remove-Item -Path "$env:USERPROFILE\AppData\Local\Cisco\Cisco Secure Client\VPN\preferences.xml" 
+
+#Remove Preference XML File from LEGACY AnyConnect "CURRENT USER" (Helps with removing old VPN url from AnyConnect AppData Path)
+Remove-Item -Path "$env:USERPROFILE\AppData\Local\Cisco\Cisco AnyConnect Secure Mobility Client\preferences.xml"
